288afc437984429c3b4101b35d23f1db9722807b
Subhadeep Dey <SDey96@users.noreply.github.com>
2018-06-30 09:32:58 +0530

    Update projectwithusers.py

--- project/projectwithusers.py
+++ project/projectwithusers.py
@@ -4,7 +4,7 @@ from flask import request, redirect, url_for, jsonify, flash
 from itertools import zip_longest
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
-from database_setupwithusers import Base, Category, Item, User
+from .database_setupwithusers import Base, Category, Item, User
 
 from flask import session as login_session
 import random
@@ -27,8 +27,10 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 app = Flask(__name__)
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 CLIENT_ID = json.loads(
-    open('client_secrets.json', 'r').read())['web']['client_id']
+    open(APP_ROOT + '/client_secrets.json', 'r').read())['web']['client_id']
+
 APPLICATION_NAME = "Item Catalog with users Application"
 
 
@@ -44,7 +46,8 @@ def gconnect():
 
     try:
         # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow = flow_from_clientsecrets(
+            APP_ROOT + '/client_secrets.json', scope='')
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
